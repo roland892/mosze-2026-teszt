89d012dbe6de6f80bbb3cc49631b278ba92f3afa
roland892 <rol7yka@gmail.com>
2026-03-02 14:44:33 +0100

    Fix syntax errors and initialize variables in main.cpp
    Javított Kód

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hiányzik a _
+    int *b = new int[N_ELEMENTS]; // hiányzik a _
     std::cout << '1-100 ertekek duplazasa'// az aposztróf helyett "" kell és a végén kimaradt a ;
-    for (int i = 0;) //nem teljes a ciklusmag
+    for (int i = 0; i<N_ELEMENTS,i++) //nem teljes a ciklusmag
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" // a végén hiányzik a ;
+        std::cout << "Ertek:"; // a végén hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // nincsen nullázva
-    for (int i = 0; i < N_ELEMENTS, i++) // a vessző helyett ;
+    int atlag = 0; // nincsen nullázva
+    for (int i = 0; i < N_ELEMENTS; i++) // a vessző helyett ;
     {
-        atlag += b[i] // kell a végére a ;
+        atlag += b[i]; // kell a végére a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
 
